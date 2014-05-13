34e950a92a8fe48e8eae3f2cff2895a1ec1aa020
Sarang Joshi <sarangj@Sarangs-MacBook-Air.local>
2014-05-13 18:20:09 -0500

    puts authors in csv

--- author_scrape.py
+++ author_scrape.py
@@ -1,22 +1,34 @@
 
 import praw
 import time
-
+import csv
 
 user_name = 'smoovewill'
 
 r = praw.Reddit(user_agent = user_name)
 
 thing_limit = 100
-authors = []
+
 
 num = 10
 
 for i in xrange(num):
+    authors = []
     comments = r.get_comments('all')
     for comment in comments:
         authors.append(comment.author)
+    
+    with open('authors.csv','a') as f:
+        writeit = csv.writer(f)
+        for author in authors:
+            writeit.writerow([author])
     time.sleep(2)
 
-print authors[0]
-print authors[:3]
+#print authors[0]
+#print authors[:3]
+
+
+
+
+
+    
