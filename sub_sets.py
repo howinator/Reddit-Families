4617a06375b3ed29ac2d184881dbd816f6ae05cc
HowieBenefiel <hben592@gmail.com>
2014-05-21 10:42:52 -0500

    Added the line to save the numpy array. I am now starting the script.

--- sub_sets.py
+++ sub_sets.py
@@ -8,6 +8,9 @@ import numpy as np
 import time
 import networkx as nx
 from matplotlib import pyplot as plt
+from tempfile import TemporaryFile
+
+numpy_saved = TemporaryFile()
 
 #print repr(open('authors.csv','rb').read(200))
 '''
@@ -27,7 +30,7 @@ users = ['' for i in xrange(len(u))]
 for i in xrange(len(u)):
     users[i] = u[i].strip()
         
-n = 1  #number of users to consider
+n = 8000  #number of users to consider
 
 print users[:n]
 
@@ -120,7 +123,7 @@ for name in users[:n]:
         NuUp[j] = comment.ups
         
         j += 1
-print NuBo[:]
+np.save(numpy_saved, CommInfo)
 #subs = set().union(*user_subs)
 #generate set of subreddits
 subs = set([])
