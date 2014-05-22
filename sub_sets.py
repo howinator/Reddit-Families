4dec87784447f3816ab5f71986e04647c528515b
Paul (Howie) Benefiel <hben592@gmail.com>
2014-05-22 00:36:20 -0500

    So, the SQL database has now been implemented for comments. I think I'm going to go ahead and fire this up on my desktop.

--- sub_sets.py
+++ sub_sets.py
@@ -8,9 +8,8 @@ import numpy as np
 import time
 import networkx as nx
 from matplotlib import pyplot as plt
-from tempfile import TemporaryFile
-
-numpy_saved = TemporaryFile()
+import sys
+import sqlite3 as lite
 
 #print repr(open('authors.csv','rb').read(200))
 '''
@@ -30,23 +29,17 @@ users = ['' for i in xrange(len(u))]
 for i in xrange(len(u)):
     users[i] = u[i].strip()
         
-<<<<<<< HEAD
 n = 10  #number of users to consider
-=======
-n = 8000  #number of users to consider
->>>>>>> 4617a06375b3ed29ac2d184881dbd816f6ae05cc
 
 print users[:n]
 
 user_name = 'smoovewill'
 
 r = praw.Reddit(user_agent = user_name)
-
 #dictionarywuh users as keys and set of subreddits as values
 user_subs = dict()
 i = 0
-j = 0
-
+k = 0
 
 #generate dict of users and sets of subreddits
 for name in users[:n]:
@@ -58,11 +51,64 @@ for name in users[:n]:
     # before the loop was started. I also took out the sleep command
     # because I learned PRAW takes care of the API limit. -Howie
     comments = user.get_comments(limit = 100)
-    for comment in comments:
-        user_subs[name].add(comment.subreddit.display_name)
+    try:
+        con = lite.connect('redditdata.db')
         
-        # 'distinguished', 'likes', 'num_reports' may be NoneTypes, so they
-        # must be converted to int
+        cur = con.cursor()
+
+        cur.execute("DROP TABLE IF EXISTS Comments")
+        cur.execute("""CREATE TABLE Comments (total_num INT, num_4_user INT, 
+            author TEXT, body TEXT, created REAL, 
+            created_utc REAL, distinguished INT, downs INT, edited INT, 
+            gilded INT, id TEXT, likes INT, link_author TEXT,  
+            link_id TEXT, link_title TEXT, link_url TEXT, name INT, 
+            num_reports INT, parent_id TEXT, subreddit_name TEXT, 
+            subreddit_id TEXT, ups INT)""")
+        j = 0
+        for comment in comments:
+            user_subs[name].add(comment.subreddit.display_name)
+            SBo = comment.body
+            SCr = comment.created
+            SCU = comment.created_utc
+            SDi = int(0 if comment.distinguished is None else 
+                      comment.distinguished)
+            SDo = comment.downs
+            SEd = int(0 if comment.edited is False else 1)
+            SGi = comment.gilded
+            SId = comment.id
+            SLi = int(0 if comment.likes is None else comment.likes)
+            SLA = comment.link_author
+            SLI = comment.link_id
+            SLT = comment.link_title
+            SLU = comment.link_url
+            SNa = comment.name
+            SNR = int(0 if comment.num_reports is None else 
+                      comment.num_reports)
+            SPI = comment.parent_id
+            SSDN = comment.subreddit.display_name
+            SSI = comment.subreddit_id
+            SUp = comment.ups
+            #CommTup = (k ,j, name, SBo, SCr, SCU, SDi, SDo, SEd, SGi, SId, 
+            #           SLi, SLA, SLI, SLT, SLU, SNa, SPI, SSDN, SSI, SUp)
+            cur.execute("""INSERT INTO Comments VALUES(?, ?, ?, ?, ?, 
+                 ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
+                 (k, j, name, SBo, SCr, SCU, SDi, SDo, SEd, SGi, SId, SLi, SLA, 
+                  SLI, SLT, SLU, SNa, SNR, SPI, SSDN, SSI, SUp,))
+            j += 1
+            k += 1
+        con.commit()
+    
+    except lite.Error, e:
+        
+        if con:
+            con.rollback()
+        print "Error: %s. Table not made for user: %s" % (e.args[0], user)
+        sys.exit(1)
+    finally:
+
+        if con:
+            con.close()
+'''
 #subs = set().union(*user_subs)
 #generate set of subreddits
 subs = set([])
@@ -114,4 +160,4 @@ for i in xrange(len(sub_users.keys())):
 G = nx.to_networkx_graph(A)
 nx.draw_spring(G,node_size = 100* np.log(.00001*sizes),width = .05,labels = labels,font_size = 8,linewidths = 0)
 plt.show()
-
+'''
