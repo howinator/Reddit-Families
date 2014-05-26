64ce7edae72b22064c783d6b6fd8cfbadf8984bd
HowieBenefiel <hben592@gmail.com>
2014-05-26 12:11:02 -0500

    Cleaned the script up to remove a blank last row.

--- make_authorsSQL_from_csv.py
+++ make_authorsSQL_from_csv.py
@@ -14,23 +14,31 @@ mypass = passw[1]
 
 users = ['' for i in xrange(len(u))]
 
+j = 0
+
 for i in xrange(len(u)):
     users[i] = u[i].strip()
+    j += 1
+
+# Without this, thie last row od the csv was blank.
+users = users[:(j-1)]    
 
 con = mdb.connect('localhost', 'howieadmin', mypass, 'redditdata');
 cur = con.cursor()
 
-print mypass;
+# These two li8nes just clear the variables from memory to prevent intrusions.
+mypass = None
+passw = None
 
-j = 0
+cur.execute("DROP TABLE IF EXISTS UserNames")
+cur.execute("""CREATE TABLE UserNames(user_num INT PRIMARY KEY AUTO_INCREMENT, 
+        user_name TEXT)""")
 
 for user in users:
 
     insert_user = "INSERT INTO UserNames(user_name) VALUES (%s)"
 
     cur.execute(insert_user, (user,))
-    
-    j += 1
 
 con.commit()
 con.close()
