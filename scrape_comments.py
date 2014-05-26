d177a5f9d2f69c8f84b85e0cc9128ee011e37d94
HowieBenefiel <hben592@gmail.com>
2014-05-26 15:18:58 -0500

    Made some changes.

--- scrape_comments.py
+++ scrape_comments.py
@@ -1,5 +1,4 @@
 import SQLOps
-import csv
 import praw
 import time
 import sys
@@ -16,16 +15,11 @@ sql = SQLOps.SQLClass()
 users = sql.get_usernames(NUsersStart, NUsersStop)
 
 i = 0
-k = 0
 # This is just to find the starting point for the script.
 m = NUsersStart
 
 
-# fix this later.
-users = authors
-
 print users
-
 
 
 for name in users:
@@ -36,7 +30,7 @@ for name in users:
        j = 0
 
        for comment in comments:
-           sql.add_comm_row(k, j, m, name, comment.body, comment.created, 
+           sql.add_comm_row(j, m, name, comment.body, comment.created, 
                 comment.created_utc, comment.distinguished, comment.downs, 
                 comment.edited, comment.gilded, comment.id, comment.likes, 
                 comment.link_author, comment.link_id, comment.link_title, 
@@ -45,8 +39,8 @@ for name in users:
                 comment.subreddit_id, comment.ups)
         
            j += 1
-           k += 1
        m += 1
     except praw.requests.HTTPError:
         print name,'does not exist'
+        m += 1
          
