228fcf65d5ab185d1b6be540c77963683896f0af
Paul (Howie) Benefiel <hben592@gmail.com>
2014-06-01 14:19:35 -0500

    Committing these to checkout master.

--- gen_graph.py
+++ gen_graph.py
@@ -1,15 +1,66 @@
 import SQLOps
+from collections import Counter
+import networkx as nx
+import numpy as np
+from matplotlib import pyplot as plt
 
-sql = SQLOps.SQLClass()
+sql = SQLOps.read_SQL()
+sql.open()
+print sql.status
 
 strt = 0
-end = 2
+end = 500
 
 userlist = sql.get_usernames(strt,end)
 usersubs = dict()
+subs = set()
 for user in userlist:
-    usersubs[user] = sql.get_usersubs(user)
+    reddits = sql.get_usersubs(user)
+    usersubs[user] = [set(reddits),Counter(reddits)]
+    subs |= set(reddits)
+    
 
-print usersubs[userlist[0]]
+sql.close()
+print sql.status
+
+min = 5 #min number of comments to be considered a 'member'
+
+subusers = {sub:set() for sub in subs}
+for sub in subs:
+    for user in userlist:
+        if usersubs[user][1][sub] >= min:
+           subusers[sub].add(user)
+    if subusers[sub] == set():
+       subusers.pop(sub,None)
+
+
+
+
+#print subusers[subusers.keys()[0]]
+print len(subusers.keys())
+A = np.zeros((len(subusers.keys()),len(subusers.keys())))
+reddits = subusers.keys()
+for i in xrange(len(reddits)):
+    for j in xrange(i,len(reddits)):
+        A[i,j] += len(subusers[reddits[i]]&subusers[reddits[j]])
+        A[j,i] = A[i,j]
+
+min_size = np.array([.5 for i in xrange(len(reddits))])
+node_sizes = np.maximum(50*np.log(.05*np.sum(A,axis=1)),min_size)
+
+
+labels = dict()
+for i in xrange(len(reddits)):
+    if node_sizes[i] >= 25:
+       labels[i] = reddits[i]
+    else:
+       labels[i] = ''
+
+G = nx.to_networkx_graph(A)
+nx.draw_spring(G,node_size = node_sizes,labels = labels,font_size = 8,width = .05,linewidths = 0.05)
+plt.show()
+
+
+
 
     
